59791bb85893a95b070eb998b925d1ec078bc6a0
umar abbas <omer.ithj@gmail.com>
2020-01-05 13:08:39 +0400

    Add project files.

---
+++ BIRS/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BIRS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
