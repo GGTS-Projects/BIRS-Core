2aa2d79297b59c6ac9496d308a240d3b5e221b6a
umar abbas <omer.ithj@gmail.com>
2020-01-06 21:38:14 +0400

    BirsApi

---
+++ BIRS-api/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using BIRS_api.Models;
+using IdentityServer4.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BIRS_api.Data
+{
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(
+            DbContextOptions options,
+            IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+        {
+        }
+    }
+}
---
+++ BIRS-api/Models/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BIRS_api.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
