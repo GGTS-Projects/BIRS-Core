f6430e2579fa87379bc6bcedd2d65dba69c9bd58
umar abbas <omer.ithj@gmail.com>
2020-01-06 21:16:28 +0400

    Block

---
+++ BIRS-core/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BIRS_core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
